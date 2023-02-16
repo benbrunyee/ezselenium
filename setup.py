1bad6aa5f9091cc6e3faed5c26ddf967ca7c49b5
bacon1097 <brunyeeb@gmail.com>
2023-02-16 07:17:03 +0000

    initial commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='your-package-name',
+    version='0.1.0',
+    description='Your package description',
+    author='Your Name',
+    author_email='you@example.com',
+    url='https://github.com/yourusername/your-package',
+    packages=['your_package_name'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
